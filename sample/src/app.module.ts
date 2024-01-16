d751d3efd72370806d12e15dc7809cedde350654
Thanh Nguyen <vnscriptkid@gmail.com>
2024-01-17 01:28:42 +0800

    [sample] fix import

--- sample/src/app.module.ts
+++ sample/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [OrderModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
