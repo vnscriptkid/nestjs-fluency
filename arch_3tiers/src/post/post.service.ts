08ce193c5a1e616c68d8a4323bd1667e76b0a0a3
Thanh Nguyen <vnscriptkid@gmail.com>
2024-01-17 23:49:58 +0800

    [3tiers] post api

---
+++ arch_3tiers/src/post/post.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class PostService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createPostDto: CreatePostDto) {
+    return this.prisma.post.create({
+      data: {
+        title: createPostDto.title,
+        content: createPostDto.content,
+      },
+    });
+  }
+
+  findAll() {
+    return this.prisma.post.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} post`;
+  }
+
+  update(id: number, updatePostDto: UpdatePostDto) {
+    return `This action updates a #${id} post`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} post`;
+  }
+}
